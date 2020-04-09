61c4afadaca1fc4a2aca777dc638f57d95cf1471
Lukuok <aaaacin@naver.com>
2020-04-09 18:09:20 +0900

    server update

--- 01_TEST/hello.py
+++ 01_TEST/hello.py
@@ -1,1 +1,22 @@
 print("Hello py")
+
+test  = 11;
+words = '11';
+
+print(words);
+
+if words is "11":
+    print('111입니다.')
+elif words is 11:
+    print('숫자 11입니다')
+else:
+    print('아무것도 아닙니다.')
+
+ㅁ = [111];
+print(ㅁ)
+
+arr = ['a','b','c','d','e','f']
+arr.append([111,222,333])
+del arr[2:4]
+
+print(arr)

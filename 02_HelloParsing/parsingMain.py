6dcbfe80c29f2df327b830f8d430a897122b9902
Lukuok <aaaacin@naver.com>
2020-12-28 13:11:36 +0900

    update

--- 02_HelloParsing/parsingMain.py
+++ 02_HelloParsing/parsingMain.py
@@ -5,13 +5,13 @@ kccCsvSrc   = "orderCsv/KCC/report_KCC.csv"
 
 m3EocSrc    = "orderEoc/3M/report_3M.eoc"
 
-orderParsingModule.xls2Csv(kccExcelSrc, kccCsvSrc)
-content = orderParsingModule.csv2String(kccCsvSrc, ',')
-order = orderParsingModule.string2ObjectForKCC(content)
-
-print(order)
-
-# content = orderParsingModule.eoc2String(m3EocSrc, '\t')
-# order = orderParsingModule.string2ObjectFor3M(content)
+# orderParsingModule.xls2Csv(kccExcelSrc, kccCsvSrc)
+# content = orderParsingModule.csv2String(kccCsvSrc, ',')
+# order = orderParsingModule.string2ObjectForKCC(content)
 
 # print(order)
+
+content = orderParsingModule.eoc2String(m3EocSrc, '\t')
+order = orderParsingModule.string2ObjectFor3M(content)
+
+print(order)
